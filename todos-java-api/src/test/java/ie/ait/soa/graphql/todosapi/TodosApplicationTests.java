9bb4ad34249e41276a17a8aff8dc095fdb2acd6f
Berchris Requiao <35544241+brneto@users.noreply.github.com>
2019-10-22 00:14:41 +0100

    Update TodosApplicationTests.java

--- todos-java-api/src/test/java/ie/ait/soa/graphql/todosapi/TodosApplicationTests.java
+++ todos-java-api/src/test/java/ie/ait/soa/graphql/todosapi/TodosApplicationTests.java
@@ -2,8 +2,11 @@ package ie.ait.soa.graphql.todosapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
+// https://github.com/graphql-java-kickstart/graphql-spring-boot/issues/230
+// https://github.com/graphql-java-kickstart/graphql-spring-boot/issues/113
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class TodosApplicationTests {
 
   @Test

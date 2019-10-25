a6c922f1e3288298ebff1a7606d452bb446264bf
Berchris Requiao <berchris.requiao@ericsson.com>
2019-10-26 00:09:48 +0100

    Add log info to every service method

--- todos-java-api/src/main/java/ie/ait/soa/graphql/todosapi/service/TodoService.java
+++ todos-java-api/src/main/java/ie/ait/soa/graphql/todosapi/service/TodoService.java
@@ -7,6 +7,7 @@ import ie.ait.soa.graphql.todosapi.model.Todo;
 import ie.ait.soa.graphql.todosapi.repository.TodoRepository;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,26 +15,36 @@ import org.springframework.transaction.annotation.Transactional;
 public class TodoService {
 
   private final TodoRepository todoRepository;
+  private final Logger logger;
 
-  TodoService(final TodoRepository todoRepository) { this.todoRepository = todoRepository; }
+  TodoService(final TodoRepository todoRepository, final Logger logger) {
+    this.todoRepository = todoRepository;
+    this.logger = logger;
+  }
 
   @Transactional
   public Todo createTodo(final String text) {
+    logger.info("{}.createTodo called with text = {}", this.getClass().getSimpleName(), text);
     return todoRepository.save(new Todo(text));
   }
 
   @Transactional
   public Optional<Todo> toggleTodoCompleted(final Long id) {
+    logger.info("{}.toggleTodoCompleted called with id = {}", this.getClass().getSimpleName(), id);
     return todoRepository
         .findById(id)
         .map(Todo::toggleCompleted);
   }
 
   @Transactional(readOnly = true)
-  public Optional<Todo> getTodo(final Long id) { return todoRepository.findById(id); }
+  public Optional<Todo> getTodo(final Long id) {
+    logger.info("{}.getTodo called with id = {}", this.getClass().getSimpleName(), id);
+    return todoRepository.findById(id);
+  }
 
   @Transactional(readOnly = true)
   public List<Todo> getTodos(final Long limit) {
+    logger.info("{}.getTodos called with limit = {}", this.getClass().getSimpleName(), limit);
     return todoRepository.findAll()
         .stream()
         .limit(ofNullable(limit).isPresent() ? limit : Long.MAX_VALUE)
